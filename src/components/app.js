3dadafb19ab2f88245379bf2aef874009577673c
Koustubh Chakraborty <kchakraborty@deloitte.com>
2019-09-18 10:07:07 +0530

    Routing fixed

--- src/components/app.js
+++ src/components/app.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import { 
-    BrowserRouter as Router,
-    Route,
-    Redirect
+    BrowserRouter as Router
 } from 'react-router-dom';
 import Navigation from './Navigation/navigation';
-import Register from "./Register/register";
-import Home from "./Home/home";
-import Contact from "./Contact/contact";
-import About from "./About/about";
-import PlayersList from './PlayersList/PlayersList';
+import PageComponents from "./Page/PageComponents";
 
 
-const App = () => (
-    <Router>
-        <Navigation />
-        <Redirect exact from="/" to="home" />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/players" component={PlayersList} />
-    </Router>
-);
+
+class App extends React.Component {
+    
+    render() {
+        return ( 
+            <Router>
+                <Navigation />
+                <PageComponents />
+            </Router>
+        )
+    }
+};
 export default App;
